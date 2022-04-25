eb4ba01c1bfb50697d82218217cd5b2c147a27d5
Nicole Franklin <Ncfranklin14@gmail.com>
2022-04-24 20:15:24 -0700

    passed tests

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,1 +1,14 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
+class Intern extends Employee {
+    constructor(name,id,email,role){
+        super(name,id,email);
+        this.school="UCLA";
+        this.role="Intern";
+    }
+    getSchool(){
+        return this.school;
+    }
+}
+
+module.exports = Intern;
